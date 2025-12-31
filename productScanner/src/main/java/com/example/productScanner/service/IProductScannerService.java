116ee82c2aa1ebd7b6d98b81d9569920cde0aa7a
Walter Baya <56239454+walterbaya@users.noreply.github.com>
2025-12-31 20:11:13 -0300

    Se eliminan carpetas innecesarias
    Las carpetas formaban parte de lo que fue la prueba utilizando microservicios, las cuales pasaran a otra rama.

--- productScanner/src/main/java/com/example/productScanner/service/IProductScannerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.productScanner.service;
-
-import com.example.productScanner.dto.ScanRequest;
-
-public interface IProductScannerService {
-    void increaseStockInOneUnit(ScanRequest scanRequest);
-}
