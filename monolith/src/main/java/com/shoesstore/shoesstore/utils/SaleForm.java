591dbfe85d3d81d7b42c39fcb897c5af04cedf0c
walterbaya <walterbaya@yahoo.com>
2026-01-05 15:18:33 -0300

    se elimina lombok de pom.xml y tambien se elimina de utils

--- monolith/src/main/java/com/shoesstore/shoesstore/utils/SaleForm.java
+++ monolith/src/main/java/com/shoesstore/shoesstore/utils/SaleForm.java
@@ -2,15 +2,11 @@ package com.shoesstore.shoesstore.utils;
 
 import com.shoesstore.shoesstore.model.*;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+
 public class SaleForm {
     private String channel;
     private List<SaleItemForm> saleItems = new ArrayList<>();
@@ -18,4 +14,41 @@ public class SaleForm {
     private double shippingCost;
     private double discountPercentage;
     private double total;
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public List<SaleItemForm> getSaleItems() {
+		return saleItems;
+	}
+	public void setSaleItems(List<SaleItemForm> saleItems) {
+		this.saleItems = saleItems;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public double getShippingCost() {
+		return shippingCost;
+	}
+	public void setShippingCost(double shippingCost) {
+		this.shippingCost = shippingCost;
+	}
+	public double getDiscountPercentage() {
+		return discountPercentage;
+	}
+	public void setDiscountPercentage(double discountPercentage) {
+		this.discountPercentage = discountPercentage;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+    
 }
--- monolith/src/main/java/com/shoesstore/shoesstore/utils/SaleItemForm.java
+++ monolith/src/main/java/com/shoesstore/shoesstore/utils/SaleItemForm.java
@@ -1,15 +1,29 @@
 package com.shoesstore.shoesstore.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class SaleItemForm {
     private Long productId;
     private Integer quantity;
+    
+    public SaleItemForm() {
+    	
+    }
+    
+	public SaleItemForm(Long productId, Integer quantity) {
+		super();
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+     
 }
