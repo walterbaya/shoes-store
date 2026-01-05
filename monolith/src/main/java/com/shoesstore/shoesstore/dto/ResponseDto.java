f239c767f0880df19a7e342fe7bdb53a32546ec5
walterbaya <walterbaya@yahoo.com>
2026-01-05 16:08:55 -0300

    se aelimina lombok de dto/ResponseDto y dto/ScanRequest

--- monolith/src/main/java/com/shoesstore/shoesstore/dto/ResponseDto.java
+++ monolith/src/main/java/com/shoesstore/shoesstore/dto/ResponseDto.java
@@ -1,11 +1,56 @@
 package com.shoesstore.shoesstore.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@AllArgsConstructor
-@Data
 public class ResponseDto {
     private String statusCode;
     private String statusMessage;
+
+    public ResponseDto() {
+    	
+    }
+    
+    public ResponseDto(String statusCode, String statusMessage) {
+    	this.statusCode = statusCode;
+    	this.statusMessage = statusMessage;
+    }
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(statusCode, statusMessage);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResponseDto other = (ResponseDto) obj;
+		return Objects.equals(statusCode, other.statusCode) && Objects.equals(statusMessage, other.statusMessage);
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseDto [statusCode=" + statusCode + ", statusMessage=" + statusMessage + "]";
+	}
+    
 }
--- monolith/src/main/java/com/shoesstore/shoesstore/dto/ScanRequest.java
+++ monolith/src/main/java/com/shoesstore/shoesstore/dto/ScanRequest.java
@@ -1,10 +1,47 @@
 package com.shoesstore.shoesstore.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@AllArgsConstructor
-@Data
 public class ScanRequest {
 	private Long id;
+
+	public ScanRequest() {
+		
+	}
+	
+	public ScanRequest(Long id) {
+		super();
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ScanRequest other = (ScanRequest) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "ScanRequest [id=" + id + "]";
+	}
+	
 }
