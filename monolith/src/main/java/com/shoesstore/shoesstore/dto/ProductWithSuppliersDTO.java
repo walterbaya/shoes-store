d4ee26d0e0892b451d4792bfd57ddc82953c7374
walterbaya <walterbaya@yahoo.com>
2026-01-05 16:06:16 -0300

    se aelimina lombok de dto/ProductWithSuppliersDTO

--- monolith/src/main/java/com/shoesstore/shoesstore/dto/ProductWithSuppliersDTO.java
+++ monolith/src/main/java/com/shoesstore/shoesstore/dto/ProductWithSuppliersDTO.java
@@ -1,20 +1,13 @@
 package com.shoesstore.shoesstore.dto;
 
 import com.shoesstore.shoesstore.model.Product;
+import com.shoesstore.shoesstore.model.Product.Gender;
+import com.shoesstore.shoesstore.model.Product.ShoeSize;
 import com.shoesstore.shoesstore.model.Supplier;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ProductWithSuppliersDTO {
     private Long id;
     private Long name;
@@ -23,14 +16,18 @@ public class ProductWithSuppliersDTO {
     private String type;
     private String material;
     private String brand;
-    private com.shoesstore.shoesstore.model.ShoeSize size;
+    private ShoeSize size;
     private Double price;
     private Integer stock;
     private Set<String> supplierNames = new HashSet<>();
     private LocalDateTime createdAt;
-    private com.shoesstore.shoesstore.model.Gender gender;
+    private Gender gender;
 
-    // Constructor que acepta un Product
+    
+    public ProductWithSuppliersDTO() {
+    	
+    }
+    
     public ProductWithSuppliersDTO(Product product) {
         this.id = product.getId();
         this.name = product.getName();
@@ -51,6 +48,8 @@ public class ProductWithSuppliersDTO {
             this.supplierNames.add(supplier.getName());
         }
     }
+    
+    
 
     // Getters y setters
     public String getFormattedPrice() {
@@ -64,4 +63,92 @@ public class ProductWithSuppliersDTO {
     public String getGenderDisplay() {
         return gender.getDisplayValue();
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getName() {
+		return name;
+	}
+
+	public void setName(Long name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Set<String> getSupplierNames() {
+		return supplierNames;
+	}
+
+	public void setSupplierNames(Set<String> supplierNames) {
+		this.supplierNames = supplierNames;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}    
 }
